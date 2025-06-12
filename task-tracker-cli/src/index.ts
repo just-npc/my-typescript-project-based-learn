1efa8741bb3b5a91e6db3f993c58aa49f2e8b3b9
riiiido <121432050+just-npc@users.noreply.github.com>
2025-06-12 21:31:31 +0700

    add mark task and show task feature
    there's mark by task done and in progress also show with filtering based on its mark

--- task-tracker-cli/src/index.ts
+++ task-tracker-cli/src/index.ts
@@ -1,17 +1,37 @@
 #!/usr/bin/env node
 import * as fs from "fs";
-import {handleAddTask, handleDeleteTask, handleUpdateTask, TaskType} from "./utils/utils";
+import {
+  handleAddTask,
+  handleDeleteTask,
+  handleUpdateTask,
+  markTaskDone,
+  markTaskInProgress, showDoneTask, showInProgressTask, showTaskList, showTodoTask,
+  TaskType
+} from "./utils/utils";
+
 
 const args = process.argv.slice(2);
-
-if (args.length < 2) {
-  console.log('Usage: task-cli add "Your Task" ');
-  process.exit(1);
-}
 
 const command = args[0];
 const content = args.slice(1).join(" ");
 const file = "./task.json";
+enum CommandType {
+  add = "add",
+  update = "update",
+  delete = "delete",
+  markInProgress = "mark-in-progress",
+  markDone = "mark-done",
+  list = "list",
+  listDone = "list-done",
+  listTodo = "list-todo",
+  listInProgress = "list-in-progress",
+}
+const commandCheck: boolean = command === CommandType.add || command === CommandType.update || command === CommandType.delete || command === CommandType.markDone || command === CommandType.markInProgress;
+
+if (commandCheck && args.length < 2) {
+  console.log('Usage: task-cli add "Your Task" ');
+  process.exit(1);
+}
 
 if (!fs.existsSync(file)) {
   fs.writeFileSync(file, "[]", "utf-8");
@@ -24,16 +44,42 @@ const  index: number = tasks.findIndex((task) => task.id === taskId);
 const  newTaskDescription: string = args.slice(2).join(" ");
 
 switch (command) {
-  case "add":
+  case CommandType.add:
     handleAddTask(tasks, content);
     break;
 
-  case "update":
+  case CommandType.update:
     handleUpdateTask(index, newTaskDescription, tasks);
     break;
 
-  case "delete":
+  case CommandType.delete:
     handleDeleteTask(index, tasks);
     break;
 
+  case CommandType.markInProgress:
+    markTaskInProgress(index, tasks);
+    break;
+
+  case CommandType.markDone:
+    markTaskDone(index, tasks);
+    break;
+
+  case CommandType.list:
+    showTaskList();
+    break;
+
+  case CommandType.listDone:
+    showDoneTask();
+    break;
+
+  case CommandType.listTodo:
+    showTodoTask();
+    break;
+
+  case CommandType.listInProgress:
+    showInProgressTask();
+    break;
+
+  default:
+    console.log("write the right command! \n use 'task-cli help' to see command list");
 }
--- task-tracker-cli/src/utils/utils.ts
+++ task-tracker-cli/src/utils/utils.ts
@@ -27,8 +27,6 @@ export function handleAddTask(tasks: TaskType[], content: string): void {
 }
 
 export function handleUpdateTask(index: number, newTaskDescription: string, tasks: TaskType[]): void {
-  // const  index: number = tasks.findIndex((task) => task.id === taskId);
-
   if (index !== -1) {
     tasks[index].description = newTaskDescription;
     tasks[index].updatedAt = new Date().toISOString();
@@ -38,11 +36,53 @@ export function handleUpdateTask(index: number, newTaskDescription: string, task
   console.log(tasks);
 }
 
-export function  handleDeleteTask(index: number, tasks: TaskType[]): void {
+export function handleDeleteTask(index: number, tasks: TaskType[]): void {
   if (index !== -1) {
     tasks.splice(index, 1);
   }
 
   fs.writeFileSync("task.json", JSON.stringify(tasks, null, 2), "utf-8");
   console.log("🗑️  Task removed");
+}
+
+export function markTaskInProgress(index: number, tasks: TaskType[]): void {
+  if (index !== -1) {
+    tasks[index].status = "in-progress";
+  }
+
+  fs.writeFileSync("task.json", JSON.stringify(tasks, null, 2), "utf-8");
+  console.log("👨‍💻  Task marked as in-progress");
+}
+
+export function markTaskDone(index: number, tasks: TaskType[]): void {
+  if (index !== -1) {
+    tasks[index].status = "done";
+  }
+
+  fs.writeFileSync("task.json", JSON.stringify(tasks, null, 2), "utf-8");
+  console.log("✔️  Task marked as done");
+}
+
+const taskList = fs.readFileSync("task.json", "utf8");
+
+export function showTaskList(): void {
+  console.log(taskList);
+}
+
+export function showDoneTask(): void {
+  const task = JSON.parse(taskList);
+  const doneTask = task.filter((task: TaskType) => task.status === "done");
+  console.log(doneTask);
+}
+
+export function showTodoTask(): void {
+  const task = JSON.parse(taskList);
+  const todoTask = task.filter((task: TaskType) => task.status === "todo");
+  console.log(todoTask);
+}
+
+export function showInProgressTask(): void {
+  const task = JSON.parse(taskList);
+  const inProgressTask = task.filter((task: TaskType) => task.status === "in-progress");
+  console.log(inProgressTask);
 }
