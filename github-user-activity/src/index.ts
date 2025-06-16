c9239a12b2d1a20a722b721579364f0015e651b2
riiiido <121432050+just-npc@users.noreply.github.com>
2025-06-16 19:05:41 +0700

    add some feature

--- github-user-activity/src/index.ts
+++ github-user-activity/src/index.ts
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 
 const inputMap: Record<string, string> = {
   commit: "PushEvent",
-  issues:"IssuesEvent",
+  issues: "IssuesEvent",
   stars: "WatchEvent",
   comment: "IssueCommentEvent",
   pr: "PullRequestEvent",
@@ -20,22 +20,24 @@ const githubUsername: string = args[1];
 async function getUserActivity(username: string): Promise<void> {
   try {
     const response = await fetch(
-        `https://api.github.com/users/${username}/events/public`
+      `https://api.github.com/users/${username}/events/public`
     );
     const events: any[] = await response.json();
 
-
-    events.forEach(event => {
-
+    events.forEach((event) => {
       if (event.type !== selectedEvent) return;
 
       switch (event.type) {
         case "PushEvent":
-          console.log(`Pushed ${event.payload.commits.length} commit to ${event.repo.name}`);
+          console.log(
+            `Pushed ${event.payload.commits.length} commit to ${event.repo.name}`
+          );
           break;
 
         case "CreateEvent":
-          console.log(`Created ${event.payload.ref_type} "${event.payload.ref}"`);
+          console.log(
+            `Created ${event.payload.ref_type} "${event.payload.ref}"`
+          );
           break;
 
         case "WatchEvent":
@@ -43,10 +45,37 @@ async function getUserActivity(username: string): Promise<void> {
           break;
 
         case "IssuesEvent":
-          console.log(`${event.payload.action} issue with #${event.payload.issue.number}`);
+          console.log(
+            `${event.payload.action} issue #${event.payload.issue.number}: "${event.payload.title}"`
+          );
+          break;
+
+        case "PullRequestEvent":
+          console.log(
+            `${event.payload.action} pull request #${event.payload.pull_request.number} in ${event.repo.name}/${event.payload.pull_request.title}`
+          );
+          break;
+
+        case "ForkEvent":
+          console.log(
+            `Forked ${event.repo.name} to ${event.payload.forkee.full_name}`
+          );
+          break;
+
+        case "IssueCommentEvent":
+          console.log(
+            `Commented on issue #${event.payload.issue.number} in ${event.repo.name}`
+          );
+          break;
+
+        case "ReleaseEvent":
+          console.log(
+            `${event.payload.release.tag_name} in ${event.repo.name}`
+          );
           break;
 
         default:
+          console.log("nothing found!");
           break;
       }
     });
