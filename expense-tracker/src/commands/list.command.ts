7e2ea50d6a13b05b8f65abea30b6c56e5f25d46f
riiiido <121432050+just-npc@users.noreply.github.com>
2025-06-23 20:27:09 +0700

    add list with category filter feature

--- expense-tracker/src/commands/list.command.ts
+++ expense-tracker/src/commands/list.command.ts
@@ -3,19 +3,39 @@ import {expenses, TrackerType} from "../utils/utils";
 import { Command } from "commander";
 
 const table = new Table({
-  head: [ "ID", "Date", "Description", "Amount" ],
-  colWidths: [ 5, 12, 40, 10 ]
+  head: [ "ID", "Date", "Description", "Category", "Amount" ],
+  colWidths: [ 5, 12, 40, 12, 10 ]
 });
 
 expenses.map((expense: TrackerType) => {
-  table.push([expense.id, expense.date, expense.description, expense.amount]);
+  table.push([expense.id, expense.date, expense.description, expense.category, expense.amount]);
 });
+
+const filteredTable = new Table({
+  head: [ "ID", "Date", "Description", "Category", "Amount" ],
+  colWidths: [ 5, 12, 40, 12, 10 ]
+})
 
 export const listExpense = new Command()
     .command("list")
     .description("List expense")
-    .action(() => {
-      console.log(table.toString());
+    .option("--category <category>", "to see list with category filter")
+    .action((option) => {
+      if (option.category) {
+        const filteredData: TrackerType[] = expenses.filter((item) => item.category === option.category);
+        filteredData.forEach((item: TrackerType) => {
+          filteredTable.push([
+              item.id,
+              item.date,
+              item.description,
+              item.category,
+              item.amount,
+          ]);
+        });
+        console.log(filteredTable.toString());
+      } else {
+        console.log(table.toString());
+      }
     });
 
 
